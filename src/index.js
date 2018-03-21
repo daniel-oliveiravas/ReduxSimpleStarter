75f11a54d70c9bf38aaae6d18ff5433ca884282d
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-03-20 22:14:27 -0300

    Set state of App component with videos list after the youtube search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const YOUTUBE_API_KEY = 'AIzaSyA3qfOeHHQ8lITPtSt99aLddoOWUwZ8mIc';
 
-const App = () => {
-  return <div>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return <div>
       <SearchBar />
     </div>
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
