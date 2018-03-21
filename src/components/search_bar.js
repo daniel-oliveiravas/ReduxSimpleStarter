bda943b76e1aa2557089b285b48611400bad4271
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-03-20 23:10:45 -0300

    Add some application style

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,11 +9,13 @@ class SearchBar extends Component {
   }
 
   render() {
-    return <div>
-      <input
-        value={this.state.term}
-        onChange={event => this.setState({ term: event.target.value })} />
-    </div>
+    return (
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
