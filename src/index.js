70b6c0b0d5aed5ee484c2e2ca5d2909df425de61
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-03-20 21:20:32 -0300

    Create youtube data api key constant

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const YOUTUBE_API_KEY = 'AIzaSyA3qfOeHHQ8lITPtSt99aLddoOWUwZ8mIc';
+
 const App = () => {
   return <div>
         <h1>Hello World</h1>
