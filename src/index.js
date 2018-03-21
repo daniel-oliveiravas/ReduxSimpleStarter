30ed6a1ad661081320ab51fc44164c8ec1643571
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2018-03-20 23:16:58 -0300

    Search new videos based on the input of search bar component

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({ key: YOUTUBE_API_KEY, term: 'surfboards' }, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: YOUTUBE_API_KEY, term: term }, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -27,7 +31,7 @@ class App extends Component {
 
   render() {
     return <div>
-      <SearchBar />
+      <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
       <VideoDetail video={this.state.selectedVideo} />
       <VideoList
         onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
